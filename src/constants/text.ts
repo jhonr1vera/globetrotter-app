278384e177bdf2346f0e8fab1db4697e74bb2ad4
Jhon Rivera <jhonariverab@gmail.com>
2025-09-14 21:33:14 -0400

    add: app texts

---
+++ src/constants/text.ts
@@ -0,0 +1,59 @@
+export const GeneralText  = {
+    company:{
+        name: "Globetrotter 🚵",
+        nameTest: "AvilaTek",
+        madeBy: "Jhon",
+        made: "Hecho por",
+        to: "Prueba para",
+    },
+    title: {
+        mainTitle: "Agenda tu vuelo con nosotros ✈️",
+        destinationTitle: "¿A Dónde Volamos?",
+        detailsTitle: "¿Viajas con alguien más?",
+        servicesTitle: "Mejora tu experiencia",
+        summaryTitle: "Así va tu Viaje"
+    },
+    label: {
+      traveler: "Viajero",
+      destination: "Destino:", 
+      departureDate: "Fecha de salida:",
+      returnDate: "Fecha de regreso:",
+      flightClass: "Clase de vuelo:",
+      price: "Precio (No incluye servicios adicionales):",
+      travelersQuantity: "Indique la cantidad de viajeros:",
+      travelersQuantityIndication: "Cantidad de viajeros:",
+      travelerName: "Nombre completo:",
+      travelerDateBirth: "Fecha de nacimiento:",
+      travelerDocumentId: "Número de documento:",
+      insurance: "¿Desea agregar seguro de viaje?",
+      preferentialSeating: "¿Deseas seleccionar asientos preferenciales?",
+      specialAssistance: "¿Requiere asistencia especial?",
+      insuranceIndication: "Seguro de viaje:",
+      preferentialSeatingIndication: "Asiento preferencial:",
+      specialAssistanceIndication: "Asistencia especial:",
+      specialAssistanceDetails: "Detalles de asistencia especial:",
+      flightCost: "Costo de vuelo:"
+    },
+    placeholder: {
+        selectDestination: "Escriba o seleccione un destino",
+        selectDestinationFirst: "Escriba o seleccione un destino primero",
+        selectClass: "Seleccione su clase:",
+        specialAssistance: "Describe brevemente tu requerimiento (No obligatorio)",
+    },
+    buttons: {
+        confirmation: "Confirmar reserva",
+        next: "Siguiente",
+        back: "Atrás",
+        exit: "Salir"
+    },
+    status: {  
+        notSpecified: "No especificado",
+        dontApply: "❌ No aplica",
+        apply: "✅ Aplica",
+        successFlight: "Vuelo agendado exitosamente ✅"
+    },
+    validation: {
+        destination: "Elige un destino valido ❌",
+        selectDestinationValid: "Escriba o seleccione un destino válido",
+    }
+}
